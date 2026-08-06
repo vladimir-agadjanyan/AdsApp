9591028fd8034460ff2ece79193ac0c7df838b13
Vladimir <vladimir,agadjanyan@gmail.com>
2026-08-06 12:09:33 +0500

    style: add application favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,15 +4,19 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="icon" type="image/png" sizes="32x32"  href="{{ asset('favicon/favicon-32x32.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon/favicon-16x16.png') }}">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('favicon/apple-touch-icon.png') }}">
+    <link rel="manifest" href="{{ asset('favicon/manifest.json') }}">
+    <link rel="shortcut icon" href="{{ asset('favicon/favicon.ico') }}">
 
     <title>
         {{ isset($title) ? $title . ' | ' : '' }}{{ config('app.name') }}
     </title>
 
     @vite(['resources/scss/app.scss', 'resources/js/app.js'])
-    
+
 </head>
 
 <body class="bg-body-tertiary">
